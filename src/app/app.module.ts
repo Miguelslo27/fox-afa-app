4eacd6a5cf4beccf1714e855083afce3bb7ad5f7
Miguel <miguelmail2006@gmail.com>
2017-06-14 11:04:22 -0300

    Merge Dev to QA (#2)
    * Merge Master to Dev
    
    * First components and settings Merge (#1)
    
    * Config and packages changes
    * Favicon updated
    * Live landing hardcoded added
    * Added first components to build the app
    * Added and templated component for Live programming list
    * Added some common components like get player button to reuse
    * Added program preview and program details
    * Added routes and updated components default prefix
    * Added .htaccess to redirect app to Angular
    * Fixed .htaccess to manage Angular URIs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ROUTING } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { ProgramPreviewComponent } from './commons/program-preview/program-preview.component';
+import { LiveComponent } from './live/live.component';
+import { ProgrammingComponent } from './programming/programming.component';
+import { PlayerComponent } from './player/player.component';
+import { BtnGetPlayerComponent } from './commons/commands/btn-get-player/btn-get-player.component';
+import { ProgramDetailsComponent } from './commons/program-preview/program-details/program-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProgramPreviewComponent,
+    LiveComponent,
+    ProgrammingComponent,
+    PlayerComponent,
+    BtnGetPlayerComponent,
+    ProgramDetailsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
